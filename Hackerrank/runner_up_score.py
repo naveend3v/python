62e8f79e067c30845cb7731e0e45d61038ab4100
naveend3v <naveenselvaraj1997@gmail.com>
2024-02-09 23:50:57 +0530

    runner_up_score

---
+++ Hackerrank/runner_up_score.py
@@ -0,0 +1,8 @@
+# https://www.hackerrank.com/challenges/find-second-maximum-number-in-a-list/problem
+
+# Find the Runner-Up Score!
+
+n = 5
+new = [2,3,6,6,5]
+new_list = sorted(set(new))
+print(new_list[-2])
