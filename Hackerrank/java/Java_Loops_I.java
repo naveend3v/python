44015b71d77cb02de590d044c75694654cf4bdac
naveend3v <naveenselvaraj1997@gmail.com>
2024-02-28 22:33:22 +0530

    added hackerrank java code

---
+++ Hackerrank/java/Java_Loops_I.java
@@ -0,0 +1,23 @@
+// https://www.hackerrank.com/challenges/java-loops-i/problem
+// Java Loops I
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
+
+public class Java_Loops_I {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+        for (int num=1;num <=10;num++){
+            System.out.printf("%d x %d = %d%n",N,num,N*num);
+        }
+        bufferedReader.close();
+    }
+}
---
+++ Hackerrank/java/javaOutputFormatting.java
@@ -0,0 +1,22 @@
+// https://www.hackerrank.com/challenges/java-output-formatting/problem
+// https://www.baeldung.com/java-printstream-printf
+// Java Output Formatting
+
+import java.util.Scanner;
+
+// Change below class name 'javaOutputFormatting' when using this code in hackerrank
+public class javaOutputFormatting {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("================================");
+        for(int i=0;i<3;i++){
+            String s1=sc.next();
+            int x=sc.nextInt();
+            //Complete this line
+            System.out.printf("%-14s %03d%n",s1,x);
+        }
+        System.out.println("================================");
+
+    }
+}
