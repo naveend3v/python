08d6416d4f166276c538dc0c0521d47301c51ada
naveend3v <naveenselvaraj1997@gmail.com>
2024-03-24 23:14:34 +0530

    new code added

---
+++ Hackerrank/java/java_currency_formatter.java
@@ -0,0 +1,26 @@
+// https://www.hackerrank.com/challenges/java-currency-formatter/problem
+// Java Currency Formatter
+
+import java.util.*;
+import java.text.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+
+        // Write your code here.
+        Locale indiaLocale = new Locale.Builder().setLanguage("en").setRegion("IN").build();
+        String us = NumberFormat.getCurrencyInstance(Locale.US).format(payment);
+        String china = NumberFormat.getCurrencyInstance(Locale.CHINA).format(payment);
+        String france = NumberFormat.getCurrencyInstance(Locale.FRANCE).format(payment);
+        String india = NumberFormat.getCurrencyInstance(indiaLocale).format(payment);
+
+        System.out.println("US: " + us);
+        System.out.println("India: " + india);
+        System.out.println("China: " + china);
+        System.out.println("France: " + france);
+    }
+}
