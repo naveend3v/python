0c5394b3f5eb9749e4b4939370849059dc230eda
naveend3v <naveenselvaraj1997@gmail.com>
2024-04-13 20:02:25 +0530

    30 Days of code

---
+++ Hackerrank/java/Java_Valid_Username_Checker.java
@@ -0,0 +1,28 @@
+// https://www.hackerrank.com/challenges/valid-username-checker/problem
+// Username validator
+
+import java.util.Scanner;
+class UsernameValidator {
+    /*
+     * Write regular expression here.
+     */
+    public static final String regularExpression = "(^\\b\\w{0}[a-zA-Z]\\w{6,28}[a-zA-Z0-9_]\\b$)";
+}
+
+// Change below class name "Java_Valid_Username_Checker" to "Solution" when running this code in HackerRank submission
+public class Java_Valid_Username_Checker {
+    private static final Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = Integer.parseInt(scan.nextLine());
+        while (n-- != 0) {
+            String userName = scan.nextLine();
+
+            if (userName.matches(UsernameValidator.regularExpression)) {
+                System.out.println("Valid");
+            } else {
+                System.out.println("Invalid");
+            }
+        }
+    }
+}
