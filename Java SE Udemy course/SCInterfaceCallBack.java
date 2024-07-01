d42d792a12b1d4517f04eebfab3803f9497d7bc3
naveend3v <naveenselvaraj1997@gmail.com>
2024-07-01 22:35:21 +0530

    interface code

---
+++ Java SE Udemy course/SCInterfaceCallBack.java
@@ -0,0 +1,51 @@
+class Store{
+    Member mem[] = new Member[100];
+    int count = 0;
+
+    public void register(Member m){
+        mem[count++] = m;
+    }
+
+    public void inviteSale(){
+        for(int i=0;i<count;i++){
+            mem[i].callBack();
+        }
+    }
+}
+
+interface Member{
+    public void callBack();
+}
+
+class Customer implements Member{
+    String customerName;
+
+    public Customer(String name){
+        this.customerName = name;
+    }
+
+    public String getCustomerName(){
+        return this.customerName;
+    }
+
+    @Override
+    public void callBack(){
+        System.out.println("Ok, I will visit " + this.customerName);
+    }
+}
+
+public class SCInterfaceCallBack {
+    public static void main(String[] args){
+        Store s1 = new Store();
+        Customer c1 = new Customer("Naveen");
+        Customer c2 = new Customer("Ravi");
+        s1.register(c1);
+        s1.register(c2);
+        s1.inviteSale();
+
+    }
+}
+
+
+
+
